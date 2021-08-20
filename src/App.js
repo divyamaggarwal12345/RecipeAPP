35e3cbf8133180a9d7fa7bfd7d2a36e25c192058
Divyam <aggdiv@gmail.com>
2021-08-20 23:46:06 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
 function App() {
+
+    const APP_ID = "582d5013";
+    const APP_KEY = "dddfa669a861f8c16eeccd1f3a3fae03";
+
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState('chicken');
+    
+     useEffect(() => {
+      getRecipes();
+     },[query])
+
+    const getRecipes = async () => {
+     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+     const data = await response.json();
+     setRecipes(data.hits);
+    }
+
+      const updateSearch = (e) => {
+         setSearch(e.target.value);
+      }
+
+      const getSearch = (e) => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('')
+      }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}></input>
+        <button className="search-button" type ="submit">Search</button>
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe => (
+        <Recipe title ={recipe.recipe.label}
+                key={recipe.recipe.label}
+                calories={recipe.recipe.calories}
+                ingredients={recipe.recipe.ingredients} 
+                image={recipe.recipe.image}/>
+      ))}
+      </div>
     </div>
   );
 }
